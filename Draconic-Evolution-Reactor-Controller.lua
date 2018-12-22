b535f0e94c4ae0d0db62cd9bb5e42e067d625076
OmegaPhi115 <45884990+OmegaPhi115@users.noreply.github.com>
2018-12-22 21:56:36 +0100

    revert merge

--- Draconic-Evolution-Reactor-Controller.lua
+++ Draconic-Evolution-Reactor-Controller.lua
@@ -1,3 +1,4 @@
+local version = "1.0"
 -- modifiable variables
 local reactorSide = "left"
 local outputfluxgateSide = "top"
@@ -9,7 +10,6 @@ local lowestFieldPercent = 25
 
 -- please leave things untouched from here on
 os.loadAPI("lib/f")
-local version = "1.1.0"
 
 -- toggleable via the monitor, use our algorithm to achieve our target field strength or let the user tweak it
 local autoInputGate = 1
@@ -17,7 +17,6 @@ local curInputGate = 222000
 
 -- monitor 
 local mon, monitor, monX, monY
-local menu = ""
 
 -- peripherals
 local reactor
@@ -60,11 +59,9 @@ mon.monitor,mon.X, mon.Y = monitor, monX, monY
 --write settings to config file
 function save_config()
   sw = fs.open("config.txt", "w")   
+  sw.writeLine(version)
   sw.writeLine(autoInputGate)
   sw.writeLine(curInputGate)
-  sw.writeLine(menu)
-  -- useless but keep it
-  sw.writeLine(version)
   sw.close()
 end
 
@@ -73,7 +70,6 @@ function load_config()
   sr = fs.open("config.txt", "r")
   autoInputGate = tonumber(sr.readLine())
   curInputGate = tonumber(sr.readLine())
-  menu = sr.readLine()
   sr.close()
 end
 
@@ -85,85 +81,76 @@ else
   load_config()
 end
 
-menu = "reactor_control"
-
 function buttons()
 
   while true do
-	-- button handler
-	sleep(0)
-	event, side, xPos, yPos = os.pullEvent("monitor_touch")
-	--if menu == "main" then
-	--end
-	if menu ~= "main" then
-		if yPos == 1 and xPos == 1 then
-			menu = "main"
-		end
-	end
-	if menu == "reactor_control" then
-		-- output gate controls
-		-- 2-4 = -1000, 6-9 = -10000, 10-12,8 = -100000
-		-- 17-19 = +1000, 21-23 = +10000, 25-27 = +100000
-		if yPos == 10 then
-		  local flowOutputfluxgate = outputfluxgate.getSignalLowFlow()
-		  if xPos >= 2 and xPos <= 4 then
-			flowOutputfluxgate = flowOutputfluxgate-1000
-		  elseif xPos >= 6 and xPos <= 9 then
-			flowOutputfluxgate = flowOutputfluxgate-10000
-		  elseif xPos >= 10 and xPos <= 12 then
-			flowOutputfluxgate = flowOutputfluxgate-100000
-		  elseif xPos >= 17 and xPos <= 19 then
-			flowOutputfluxgate = flowOutputfluxgate+100000
-		  elseif xPos >= 21 and xPos <= 23 then
-			flowOutputfluxgate = flowOutputfluxgate+10000
-		  elseif xPos >= 25 and xPos <= 27 then
-			flowOutputfluxgate = flowOutputfluxgate+1000
-		  end
-		  if flowOutputfluxgate < 0 then
-			flowOutputfluxgate = 0
-		  end
-		  outputfluxgate.setSignalLowFlow(flowOutputfluxgate)
-		end
+    -- button handler
+	sleep(0) 
+    event, side, xPos, yPos = os.pullEvent("monitor_touch")
 
-		-- input gate controls
-		-- 2-4 = -1000, 6-9 = -10000, 10-12,8 = -100000
-		-- 17-19 = +1000, 21-23 = +10000, 25-27 = +100000
-		if yPos == 8 and autoInputGate == 0 and xPos ~= 14 and xPos ~= 15 then
-		  if xPos >= 2 and xPos <= 4 then
-			curInputGate = curInputGate-1000
-		  elseif xPos >= 6 and xPos <= 9 then
-			curInputGate = curInputGate-10000
-		  elseif xPos >= 10 and xPos <= 12 then
-			curInputGate = curInputGate-100000
-		  elseif xPos >= 17 and xPos <= 19 then
-			curInputGate = curInputGate+100000
-		  elseif xPos >= 21 and xPos <= 23 then
-			curInputGate = curInputGate+10000
-		  elseif xPos >= 25 and xPos <= 27 then
-			curInputGate = curInputGate+1000
-		  end
-		  if curInputGate < 0 then
-			curInputGate = 0
-		  end
-		  inputfluxgate.setSignalLowFlow(curInputGate)
-		  save_config()
-		end
+    -- output gate controls
+    -- 2-4 = -1000, 6-9 = -10000, 10-12,8 = -100000
+    -- 17-19 = +1000, 21-23 = +10000, 25-27 = +100000
+    if yPos == 10 then
+      local flowOutputfluxgate = outputfluxgate.getSignalLowFlow()
+      if xPos >= 2 and xPos <= 4 then
+        flowOutputfluxgate = flowOutputfluxgate-1000
+      elseif xPos >= 6 and xPos <= 9 then
+        flowOutputfluxgate = flowOutputfluxgate-10000
+      elseif xPos >= 10 and xPos <= 12 then
+        flowOutputfluxgate = flowOutputfluxgate-100000
+      elseif xPos >= 17 and xPos <= 19 then
+        flowOutputfluxgate = flowOutputfluxgate+100000
+      elseif xPos >= 21 and xPos <= 23 then
+        flowOutputfluxgate = flowOutputfluxgate+10000
+      elseif xPos >= 25 and xPos <= 27 then
+        flowOutputfluxgate = flowOutputfluxgate+1000
+      end
+	  if flowOutputfluxgate < 0 then
+		flowOutputfluxgate = 0
+	  end
+      outputfluxgate.setSignalLowFlow(flowOutputfluxgate)
+    end
 
-		-- input gate toggle
-		if yPos == 8 and ( xPos == 14 or xPos == 15) then
-		  if autoInputGate == 1 then
-			autoInputGate = 0
-		  else
-			autoInputGate = 1
-		  end
-		  save_config()
-		end
-	end
+    -- input gate controls
+    -- 2-4 = -1000, 6-9 = -10000, 10-12,8 = -100000
+    -- 17-19 = +1000, 21-23 = +10000, 25-27 = +100000
+    if yPos == 8 and autoInputGate == 0 and xPos ~= 14 and xPos ~= 15 then
+      if xPos >= 2 and xPos <= 4 then
+        curInputGate = curInputGate-1000
+      elseif xPos >= 6 and xPos <= 9 then
+        curInputGate = curInputGate-10000
+      elseif xPos >= 10 and xPos <= 12 then
+        curInputGate = curInputGate-100000
+      elseif xPos >= 17 and xPos <= 19 then
+        curInputGate = curInputGate+100000
+      elseif xPos >= 21 and xPos <= 23 then
+        curInputGate = curInputGate+10000
+      elseif xPos >= 25 and xPos <= 27 then
+        curInputGate = curInputGate+1000
+      end
+	  if curInputGate < 0 then
+		curInputGate = 0
+	  end
+      inputfluxgate.setSignalLowFlow(curInputGate)
+      save_config()
+    end
+
+    -- input gate toggle
+    if yPos == 8 and ( xPos == 14 or xPos == 15) then
+      if autoInputGate == 1 then
+        autoInputGate = 0
+      else
+        autoInputGate = 1
+      end
+      save_config()
+    end
+
   end
 end
 
 function drawButtons(y)
-  -- draw + - buttons
+
   -- 2-4 = -1000, 6-9 = -10000, 10-12,8 = -100000
   -- 17-19 = +1000, 21-23 = +10000, 25-27 = +100000
 
@@ -176,6 +163,8 @@ function drawButtons(y)
   f.draw_text(mon, 26, y, "+", colors.white, colors.gray)
 end
 
+
+
 function update()
   while true do 
 
@@ -185,7 +174,6 @@ function update()
 
     -- print out all the infos from .getReactorInfo() to term
 	print("Current Version: ", version)
-	print("Current Menu: ", menu)
 
     if ri == nil then
       error("Reactor has an invalid setup")
@@ -198,80 +186,73 @@ function update()
     print("Input Gate: ", inputfluxgate.getSignalLowFlow())
 
     -- monitor output
-	-- data
+	--f.draw_text(mon, 1, 1, "O", colors.black, colors.white)
+	f.draw_text(mon, 1, 1, "Reactor Controler>>>>>>>>>>>>", colors.white, colors.green)
 
-		--status
-	local statusColor
-	statusColor = colors.red
-	if ri.status == "running" then
-		statusColor = colors.green
-	elseif ri.status == "cold" then
-		statusColor = colors.blue
-	elseif ri.status == "warming_up" then
-		statusColor = colors.orange
-	elseif ri.status == "Stopping" then
-		statusColor = colors.orange
-	end
+    local statusColor
+    statusColor = colors.red
+
+    if ri.status == "running" then
+      statusColor = colors.green
+    elseif ri.status == "cold" then
+      statusColor = colors.blue
+    elseif ri.status == "warming_up" then
+      statusColor = colors.orange
+    end
+
+    f.draw_text_lr(mon, 2, 2, 1, "Reactor Status:", string.upper(ri.status), colors.white, statusColor, colors.black)
+
+    f.draw_text_lr(mon, 2, 4, 1, "Generation:", f.format_int(ri.generationRate) .. " RF/t", colors.white, colors.lime, colors.black)
+
+    local tempColor = colors.red
+    if ri.temperature <= 5000 then tempColor = colors.green end
+    if ri.temperature >= 5000 and ri.temperature <= 6500 then tempColor = colors.orange end
+    f.draw_text_lr(mon, 2, 6, 1, "Temperature:", f.format_int(ri.temperature) .. "C", colors.white, tempColor, colors.black)
+
+    f.draw_text_lr(mon, 2, 9, 1, "Output Gate:", f.format_int(outputfluxgate.getSignalLowFlow()) .. " RF/t", colors.blue, colors.red, colors.black)
+
+    -- buttons
+    drawButtons(10)
+
+    f.draw_text_lr(mon, 2, 7, 1, "Input Gate:", f.format_int(inputfluxgate.getSignalLowFlow()) .. " RF/t", colors.orange, colors.red, colors.black)
+
+    if autoInputGate == 1 then
+      f.draw_text(mon, 14, 8, "AU", colors.white, colors.gray)
+    else
+      f.draw_text(mon, 14, 8, "MA", colors.white, colors.gray)
+      drawButtons(8)
+    end
+
+    local satPercent
+    satPercent = math.ceil(ri.energySaturation / ri.maxEnergySaturation * 10000)*.01
+
+    f.draw_text_lr(mon, 2, 11, 1, "Energy Saturation", satPercent .. "%", colors.white, colors.white, colors.black)
+    f.progress_bar(mon, 2, 12, mon.X-2, satPercent, 100, colors.blue, colors.gray)
+
+    local fieldPercent, fieldColor
+    fieldPercent = math.ceil(ri.fieldStrength / ri.maxFieldStrength * 10000)*.01
+
+    fieldColor = colors.red
+    if fieldPercent >= 50 then fieldColor = colors.green end
+    if fieldPercent < 50 and fieldPercent > 30 then fieldColor = colors.orange end
 	
-		--fuel
-	local fuelPercent, fuelColor
-	fuelPercent = 100 - math.ceil(ri.fuelConversion / ri.maxFuelConversion * 10000)*.01
-	fuelColor = colors.red
-	if fuelPercent >= 70 then fuelColor = colors.green end
-	if fuelPercent < 70 and fuelPercent > 30 then fuelColor = colors.orange end
-	
-		--temperature
-	local tempColor = colors.red
-	if ri.temperature <= 5000 then tempColor = colors.green end
-	if ri.temperature >= 5000 and ri.temperature <= 6500 then tempColor = colors.orange end
-	
-		--saturation
-	local satPercent
-	satPercent = math.ceil(ri.energySaturation / ri.maxEnergySaturation * 10000)*.01
-	
-		--field
-	local fieldPercent, fieldColor
-	fieldPercent = math.ceil(ri.fieldStrength / ri.maxFieldStrength * 10000)*.01
-	fieldColor = colors.red
-	if fieldPercent >= 50 then fieldColor = colors.green end
-	if fieldPercent < 50 and fieldPercent > 30 then fieldColor = colors.orange end
-	
-	-- print to monitor
-	if menu == "reactor_control" then
+	f.draw_text_lr(mon, 2, 14, 1, "Field Strength", fieldPercent .. "%", colors.white, fieldColor, colors.black)
+    f.progress_bar(mon, 2, 15, mon.X-2, fieldPercent, 100, fieldColor, colors.gray)
 
-		f.draw_text(mon, 1, 1, "V Reactor Controler>>>>>>>>>>>>", colors.white, colors.green)
+    local fuelPercent, fuelColor
 
-		f.draw_text_lr(mon, 2, 2, 1, "Reactor Status:", string.upper(ri.status), colors.white, statusColor, colors.black)
+    fuelPercent = 100 - math.ceil(ri.fuelConversion / ri.maxFuelConversion * 10000)*.01
 
-		f.draw_text_lr(mon, 2, 4, 1, "Generation:", f.format_int(ri.generationRate) .. " RF/t", colors.white, colors.lime, colors.black)
+    fuelColor = colors.red
 
-		f.draw_text_lr(mon, 2, 6, 1, "Temperature:", f.format_int(ri.temperature) .. "C", colors.white, tempColor, colors.black)
+    if fuelPercent >= 70 then fuelColor = colors.green end
+    if fuelPercent < 70 and fuelPercent > 30 then fuelColor = colors.orange end
 
-		f.draw_text_lr(mon, 2, 9, 1, "Output Gate:", f.format_int(outputfluxgate.getSignalLowFlow()) .. " RF/t", colors.blue, colors.red, colors.black)
+    f.draw_text_lr(mon, 2, 17, 1, "Draconium ", fuelPercent .. "%", colors.white, fuelColor, colors.black)
+    f.progress_bar(mon, 2, 18, mon.X-2, fuelPercent, 100, fuelColor, colors.gray)
 
-		-- buttons
-		drawButtons(10)
+    f.draw_text_lr(mon, 2, 19, 1, "Action: ", action, colors.gray, colors.gray, colors.black)
 
-		f.draw_text_lr(mon, 2, 7, 1, "Input Gate:", f.format_int(inputfluxgate.getSignalLowFlow()) .. " RF/t", colors.orange, colors.red, colors.black)
-
-		if autoInputGate == 1 then
-		  f.draw_text(mon, 14, 8, "AU", colors.white, colors.gray)
-		else
-		  f.draw_text(mon, 14, 8, "MA", colors.white, colors.gray)
-		  drawButtons(8)
-		end
-
-		f.draw_text_lr(mon, 2, 11, 1, "Energy Saturation", satPercent .. "%", colors.white, colors.white, colors.black)
-		f.progress_bar(mon, 2, 12, mon.X-2, satPercent, 100, colors.blue, colors.gray)
-		
-		f.draw_text_lr(mon, 2, 14, 1, "Field Strength", fieldPercent .. "%", colors.white, fieldColor, colors.black)
-		f.progress_bar(mon, 2, 15, mon.X-2, fieldPercent, 100, fieldColor, colors.gray)
-
-		f.draw_text_lr(mon, 2, 17, 1, "Draconium ", fuelPercent .. "%", colors.white, fuelColor, colors.black)
-		f.progress_bar(mon, 2, 18, mon.X-2, fuelPercent, 100, fuelColor, colors.gray)
-
-		f.draw_text_lr(mon, 2, 19, 1, "Action: ", action, colors.gray, colors.gray, colors.black)
-	end
     -- actual reactor interaction
     --
     if emergencyCharge == true then
