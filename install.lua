5995c52789f51b072c061142978a082986c62b68
OmegaPhi115 <45884990+OmegaPhi115@users.noreply.github.com>
2018-12-22 18:22:44 +0100

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,8 +2,8 @@
 --rhNbNbFE
 --Version 1.0
 
-local libURL = "https://raw.githubusercontent.com/OmegaPhi115/Draconic-Evolution-Reactor-Controller/master/lib/f.lua"
-local startupURL = "https://raw.githubusercontent.com/OmegaPhi115/Draconic-Evolution-Reactor-Controller/master/Draconic-Evolution-Reactor-Controller.lua"
+local libURL = "https://raw.githubusercontent.com/OmegaPhi115/Draconic-Evolution-Reactor-Controller/2.0/lib/f.lua"
+local startupURL = "https://raw.githubusercontent.com/OmegaPhi115/Draconic-Evolution-Reactor-Controller/2.0/Draconic-Evolution-Reactor-Controller.lua"
 local lib, startup
 local libFile, startupFile
 
